e9f53219d305052e7bb74d82cd1a9166d3e7b2f2
Thomas Adamcik <adamcik@samfundet.no>
2009-12-14 00:07:10 +0100

    Integrate listener and irc parts

--- bot.py
+++ bot.py
@@ -5,10 +5,22 @@ import asyncore
 import logging
 
 from irc import Bot
+from listener import Listener
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(message)s")
 
 bot = Bot('localhost')
+listener = Listener()
+
+def parse(line):
+    if line.startswith('@'):
+        target, line = line[1:].split(' ', 1)
+    else:
+        target = bot.config['channel']
+
+    bot.irc_command('PRIVMSG', target, line)
+
+listener.add(parse)
 
 try:
     bot.run()
