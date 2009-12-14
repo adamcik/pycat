a05ce7e08baa1a2095d6ac8ca3a663a65497f145
Thomas Adamcik <adamcik@samfundet.no>
2009-12-14 21:15:02 +0100

    Add basic message parser using blocking Popen

--- bot.py
+++ bot.py
@@ -34,6 +34,36 @@ def listen_parser(line):
     # FIXME parse commands from users?
     bot.irc.privmsg(target, message)
 
+def msg_parser(prefix, command, args):
+    user = prefix.split('!')[0]
+    target, message = args
+
+    p = subprocess.Popen('./test.sh',
+        shell=True,
+        bufsize=1024,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE)
+
+    # FIXME this will block
+    response = p.communicate(input=message.encode('utf-8'))[0]
+
+    if not response.strip():
+        return
+
+    try:
+        response = response.decode('utf-8')
+    except UnicodeDecodeError:
+        response = response.decode('iso-8859-1')
+
+    if target == bot.current_nick:
+        for line in response.split('\n'):
+            if line.strip():
+                bot.irc.privmsg(user, line)
+    else:
+        for line in response.split('\n'):
+            if line.strip():
+                bot.irc.privmsg(target, line)
+
 def join(prefix, command, args):
     bot.irc.join(channel)
 
@@ -41,6 +71,7 @@ listener.add(listen_parser)
 
 # FIXME queue events to run after register?
 bot.add('CONNECT', join)
+bot.add('PRIVMSG', msg_parser)
 
 try:
     asyncore.loop()
