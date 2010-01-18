219fe7174bb94fe40e83e3fb82a0552378457628
Thomas Adamcik <adamcik@samfundet.no>
2010-01-18 16:01:14 +0100

    Change flow of example slightly

--- example.sh
+++ example.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
+    # Print config if called with no arguments
     echo 'match = ^!'
-    exit
+else 
+    nick=$1
+    target=$2
+    source=$3
+    message=$4
+
+    echo "Hello $source! My name is $nick. We are talking in $target and you said '$message'"
 fi
-
-nick=$1
-channel=$2
-source=$3
-message=$4
-
-echo "Hello $source! My name is $nick. We are talking in $channel and you said '$message'"
