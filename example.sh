8cd5fde6e931e314b8001da9e959c5df5cb2c8f2
Thomas Adamcik <adamcik@samfundet.no>
2010-01-28 23:19:15 +0100

    Update example.sh to match --config change

--- example.sh
+++ example.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]; then
-    # Print config if called with no arguments
+if [ $# -eq 1 -a $1 = "--config" ]; then
+    # Print config if called with --config
     echo 'match = ^!'
-else 
-    nick=$1
-    target=$2
-    source=$3
-    message=$4
+else
+    if [ $# -eq 4 ]; then
+        nick=$1
+        target=$2
+        source=$3
+        message=$4
 
-    echo "Hello $source! My name is $nick. We are talking in $target and you said '$message'"
+        echo "Hello $source! My name is $nick. We are talking in $target and you said '$message'"
+    else
+        echo "Usage: $0 nick target source \"message\""
+    fi
 fi
